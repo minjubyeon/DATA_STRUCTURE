a3bdec533831d218ade3878c0b4b9fb135a71051
minjubyeon <minjubyeon444@gmail.com>
2026-04-02 13:32:48 +0900

    서식변경. 가독성 향상

--- 자료구조3주차(복습1).cpp
+++ 자료구조3주차(복습1).cpp
@@ -7,100 +7,109 @@ UnsortedType::UnsortedType() { length = 0; }
 
 // 1. appendItem
 void UnsortedType::appendItem(ItemType value) {
-    if (isFull) {
-        return;
+  if (isFull) {
+    return;
+  }
+
+  data[length] = value;
+}
+
+//***********************************************************************************
+// 2. void insertItem(int pos, ItemType value);
+
+void UnsortedType::insertItem(int pos, ItemType value) {
+  if (pos > MAX_SIZE - 1) {
+    return;
+  }
+
+  data[length] = data[pos];
+  data[pos] = value;
+  length++;
+}
+
+void UnsortedType::insertItem(int pos, ItemType value) {
+  if (pos > MAX_SIZE - 1) {
+    return;
+  }
+
+  length++;
+
+  for (int i = 1; (length-1) < pos; i++) {
+    data[length - i] = data[length];
+    if ((length - i) == pos) break;
+  }
+
+  data[pos] = value;
+}
+
+//***********************************************************************************
+// 3. void removeItem(ItemType value);
+void UnsortedType::removeItem(ItemType value){
+    for (int i = 0; i < length; i++){
+        if (data[i] == value){
+            data[i] = data[length-1];
+            length--;
+            break;
+        }
     }
 
-    data[length] = value;
-}
-
-//***********************************************************************************
-//2. void insertItem(int pos, ItemType value);
-
-void insertItem(int pos, ItemType value){
-if (pos > MAX_SIZE){
-    return;
-}
-
 }
 
 
 //***********************************************************************************
-//3. void removeItem(ItemType value);
+// 4. void updateItem(int pos, ItemType new_value);
+void UnsortedType::updateItem(int pos, ItemType new_value){
 
+    if (pos >= length) return;
+    data[pos] = new_value;
 
-
-//***********************************************************************************
-//4. void updateItem(int pos, ItemType new_value);
-
-
-
-
-//***********************************************************************************
-//5. void clear();
-
-
+}
 
 
 //***********************************************************************************
-//6.int size();
+// 5. void clear();
+void UnsortedType::clear(){
+    length = 0;
+}
+
+//***********************************************************************************
+// 6.int size();
 int UnsortedType::size() { return length; }
 
+//***********************************************************************************
+// 7. bool isFull();
 bool UnsortedType::isFull() {
-    if (length == MAX_SIZE)
-        return true;
+  if (length == MAX_SIZE)
+    return true;
 
-    else
-        return false;
+  else
+    return false;
 }
 
-
 //***********************************************************************************
-//7. bool isFull();
-
-
-
-
-
-//***********************************************************************************
-//8. bool isEmpty();
+// 8. bool isEmpty();
 bool UnsortedType::isEmpty() {
-    if (length == 0)
-        return true;
+  if (length == 0)
+    return true;
 
-    else
-        return false;
-}
-
-
-
-//***********************************************************************************
-//9. bool findItem(ItemType item);
-bool UnsortedType::findItem(ItemType item){
-for (int i = 0; i < MAX_SIZE; i++) {
-    if (data[i] == item)
-        return true;
-}
-return false;
+  else
+    return false;
 }
 
 //***********************************************************************************
-//10. ItemType getItem(int pos);
+// 9. bool findItem(ItemType item);
+bool UnsortedType::findItem(ItemType item) {
+  for (int i = 0; i < length; i++) {
+    if (data[i] == item) return true;
+  }
+  return false;
+}
 
+//***********************************************************************************
+// 10. ItemType getItem(int pos);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ItemType UnsortedType::getItem(int pos){
+    if (pos >= length) return;
+    return data[pos];
+}
 
