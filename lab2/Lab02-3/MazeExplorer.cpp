19a5923673f069a4d209de89143a206a7d267a4c
minjubyeon <minjubyeon444@gmail.com>
2026-04-05 23:58:17 +0900

    Lab02 MazeExplorer 완성

--- lab2/Lab02-3/MazeExplorer.cpp
+++ lab2/Lab02-3/MazeExplorer.cpp
@@ -25,29 +25,44 @@ bool mazeExplorer(char map[][MAZE_SIZE], location entryPoint, location exitPoint
     // For details, check the PDF slides!
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // 이동할 수 있는 방향 (위, 오른쪽, 아래, 왼쪽)
+    int moveX[4] = { -1, 0, 1, 0 };
+    int moveY[4] = { 0, 1, 0, -1 };
 
-    return false;
-}
+    // 미로 탐색 함수
+        while (!tempStack.isEmpty()) {
+            location current = tempStack.pop();
+
+            int x = current.row;
+            int y = current.col;
+
+            // 종료 지점에 도달한 경우
+            if (x == exitPoint.row && y == exitPoint.col) {
+                printLocation(current);
+                return true;
+            }
+
+            // 범위를 벗어나거나 막힌 길이거나 이미 방문한 경우
+            if (x < 0 || x >= MAZE_SIZE || y < 0 || y >= MAZE_SIZE || map[x][y] != '0') {
+                continue;
+            }
+
+            // 현재 위치를 지나간 셀로 표시하고 좌표 출력
+            map[x][y] = '.';
+            printLocation(current);
+
+            // 4가지 방향으로 이동 (역순으로 스택에 넣음)
+            for (int i = 3; i >= 0; i--) {
+                int newX = x + moveX[i];
+                int newY = y + moveY[i];
+
+                if (newX >= 0 && newX < MAZE_SIZE && newY >= 0 && newY < MAZE_SIZE && map[newX][newY] == '0') {
+                    tempStack.push({ newX, newY });
+                }
+            }
+        }
+
+        // 경로가 없을 경우
+        return false;
+
+    }
