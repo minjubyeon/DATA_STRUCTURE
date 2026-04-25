f788b8c6aa7cd623d6e6f84205f2187c25d0ca31
minjubyeon <minjubyeon444@gmail.com>
2026-04-25 17:34:07 +0900

    Lab04_2_3 dequeue까지 완성

--- lab4/Lab04-2-3/QueueType.h
+++ lab4/Lab04-2-3/QueueType.h
@@ -5,7 +5,6 @@
 //  Modified by Jeman Park on 2026/04/21.
 //
 
-
 #ifndef QUEUETYPE_H
 #define QUEUETYPE_H
 
@@ -13,179 +12,163 @@
 using namespace std;
 
 template <class ItemType>
-struct NodeType{
-    ItemType value;
-    NodeType * next;
+struct NodeType {
+  ItemType value;
+  NodeType* next;
 };
 
 template <class ItemType>
-class QueueType
-{
-    
-private:
-    NodeType<ItemType> * pFront;
-    NodeType<ItemType> * pRear;
-    
-public:
-    
-    QueueType();
-    ~QueueType();
-    
-    void clear();
-    void enqueue(ItemType newItem);
-    void dequeue(ItemType& value);
+class QueueType {
+ private:
+  NodeType<ItemType>* pFront;
+  NodeType<ItemType>* pRear;
 
-    int size() const;
-    bool isFull() const;
-    bool isEmpty() const;
-    
-    ItemType maxDequeue();
-    
-    void Print();
-    
+ public:
+  QueueType();
+  ~QueueType();
+
+  void clear();
+  void enqueue(ItemType newItem);
+  void dequeue(ItemType& value);
+
+  int size() const;
+  bool isFull() const;
+  bool isEmpty() const;
+
+  ItemType maxDequeue();
+
+  void Print();
 };
 
-
 template <class ItemType>
-QueueType<ItemType>::QueueType()
-{
-    pFront = nullptr;
-    pRear = nullptr;
+QueueType<ItemType>::QueueType() {
+  pFront = nullptr;
+  pRear = nullptr;
 }
 
 template <class ItemType>
-QueueType<ItemType>::~QueueType()
-{
-    /* Implement the function here (Lab 04-2)*/
-    // This destructor function iteratively deletes all nodes in the queue.
-    // All "NodeType" should be "deallocated" properly.
-    // It returns nothing
-    
-    
-    
-    
-    
-    
-    
-    
+QueueType<ItemType>::~QueueType() {
+  /* Implement the function here (Lab 04-2)*/
+  // This destructor function iteratively deletes all nodes in the queue.
+  // All "NodeType" should be "deallocated" properly.
+  // It returns nothing
+
+  while (!isEmpty()) {
+    NodeType* tempPtr = pFront;
+    pFront = pFront->next;
+    delete tempPtr;
+  }
 }
 
 template <class ItemType>
-void QueueType<ItemType>::clear()
-{
-    this -> ~QueueType();
+void QueueType<ItemType>::clear() {
+  this->~QueueType();
 }
 
 template <class ItemType>
-int QueueType<ItemType>::size() const
-{
-    /* Implement the function here (Lab 04-2)*/
-    // This function counts the number of nodes in the queue and returns it.
-    
-    
-    
-    
-    
-    
-    
-    
-}
+int QueueType<ItemType>::size() const {
+  /* Implement the function here (Lab 04-2)*/
+  // This function counts the number of nodes in the queue and returns it.
 
+  int length = 0;
+  NodeType<ItemType>* tempPtr = pFront;
 
-template <class ItemType>
-bool QueueType<ItemType>::isEmpty() const
-{
-    return (pFront == nullptr);
+  while (tempPtr != nullptr) {
+    tempPtr = tempPtr->next;
+    length++;
+  }
+
+  return length;
 }
 
 template <class ItemType>
-bool QueueType<ItemType>::isFull() const
-{
-    NodeType<ItemType> * newNode;
-    try {
-        newNode = new NodeType<ItemType>;
-        delete newNode;
-        return false;
-    }
-    catch (std::bad_alloc exception){
-        return true;
-    }
+bool QueueType<ItemType>::isEmpty() const {
+  return (pFront == nullptr);
 }
 
 template <class ItemType>
-void QueueType<ItemType>::enqueue(ItemType newItem)
-{
-    /* Implement the function here (Lab 04-2)*/
-    // This function enqueues "newItem" into the queue, and returns NOTHING.
-    // If the queue is already full, do nothing.
-    
-    
-    
-    
-    
-    
-    
-    
-    
+bool QueueType<ItemType>::isFull() const {
+  NodeType<ItemType>* newNode;
+  try {
+    newNode = new NodeType<ItemType>;
+    delete newNode;
+    return false;
+  } catch (std::bad_alloc exception) {
+    return true;
+  }
 }
 
 template <class ItemType>
-void QueueType<ItemType>::dequeue(ItemType& ret_value)
-{
-    
-    /* Implement the function here (Lab 04-2)*/
-    // This function dequeues the most front item in the queue, and returns it through "RETURN BY REFERENCE (ret_value).
-    // You need to set "ret_value" as the dequeue item's value to return.
-    // If the queue is empty, set "ret_value" as -1
-    
-    
-    
-    
-    
-    
-    
-    
-}
+void QueueType<ItemType>::enqueue(ItemType newItem) {
+  /* Implement the function here (Lab 04-2)*/
+  // This function enqueues "newItem" into the queue, and returns NOTHING.
+  // If the queue is already full, do nothing.
 
-
-template <class ItemType>
-ItemType QueueType<ItemType>::maxDequeue()
-{
-    
-    /* Implement the function here (Lab 04-3)*/
-    // This function removes and returns "the max value" in the queue
-    // If the queue is empty, it returns -1 and does nothing
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
-
-
-template <class ItemType>
-void QueueType<ItemType>::Print(){
-    if(isEmpty()){
-        cout << "[EMPTY QUEUE]" << endl;
+     if(isFull()){
         return;
     }
-    
-    cout << "(front)\t[";
-    NodeType<ItemType> * tempPtr = pFront;
-    
-    while(tempPtr != nullptr){
-        cout << tempPtr -> value << "\t";
-        tempPtr = tempPtr->next;
+
+    NodeType<ItemType> * newNode;
+    newNode = new NodeType<ItemType>;
+
+    newNode -> value = newItem;
+    newNode -> next = nullptr;
+
+    if(!isEmpty()){
+        pRear ->next = newNode;
     }
-    cout << "]\t(rear)" << endl;
+    else{
+        pFront = newNode;
+    }
+    pRear = newNode;
+
 }
 
+template <class ItemType>
+void QueueType<ItemType>::dequeue(ItemType& ret_value) {
+  /* Implement the function here (Lab 04-2)*/
+  // This function dequeues the most front item in the queue, and returns it
+  // through "RETURN BY REFERENCE (ret_value). You need to set "ret_value" as
+  // the dequeue item's value to return. If the queue is empty, set "ret_value"
+  // as -1
 
+  if (isEmpty()){ ret_value = -1;}
+
+  NodeType<ItemType>* tempPtr = pFront;
+  pFront = pFront -> next;
+  ret_value = tempPtr -> value;
+
+  delete tempPtr;
+
+  if(isEmpty()){
+    pRear = nullptr;
+  }
+
+
+}
+
+template <class ItemType>
+ItemType QueueType<ItemType>::maxDequeue() {
+  /* Implement the function here (Lab 04-3)*/
+  // This function removes and returns "the max value" in the queue
+  // If the queue is empty, it returns -1 and does nothing
+}
+
+template <class ItemType>
+void QueueType<ItemType>::Print() {
+  if (isEmpty()) {
+    cout << "[EMPTY QUEUE]" << endl;
+    return;
+  }
+
+  cout << "(front)\t[";
+  NodeType<ItemType>* tempPtr = pFront;
+
+  while (tempPtr != nullptr) {
+    cout << tempPtr->value << "\t";
+    tempPtr = tempPtr->next;
+  }
+  cout << "]\t(rear)" << endl;
+}
 
 #endif /* QUEUETYPE_H */
